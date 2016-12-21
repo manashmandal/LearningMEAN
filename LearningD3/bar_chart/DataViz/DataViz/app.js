bbd8cadcaafbdecc54db08232584d069c1f5d886
Manash Kumar Mandal <manashmndl@gmail.com>
2016-12-22 00:31:04 +0600

    bar chart

---
+++ LearningD3/bar_chart/DataViz/DataViz/app.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+
+var index_filepath = __dirname + '/public/index.html';
+
+app.get('/', function (req, res) {
+    console.log(index_filepath);
+    res.sendFile(index_filepath);
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
