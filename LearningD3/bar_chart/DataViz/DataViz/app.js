6a9cdb0c488d39d6e43181940e9f7429a3f56b2a
Manash Kumar Mandal <manashmndl@gmail.com>
2016-12-22 01:31:34 +0600

    added express framework for json

--- LearningD3/bar_chart/DataViz/DataViz/app.js
+++ LearningD3/bar_chart/DataViz/DataViz/app.js
@@ -2,10 +2,21 @@
 var app = express();
 
 var index_filepath = __dirname + '/public/index.html';
+var data_filepath = __dirname + '/public/rat-data.csv';
+var data = require('./public/data.json');
 
 app.get('/', function (req, res) {
     console.log(index_filepath);
     res.sendFile(index_filepath);
+});
+
+app.get('/public/rat-data.csv', function (req, res) {
+    res.attachment(data_filepath);
+    res.sendFile(data_filepath);
+});
+
+app.get('/public/data', function (req, res) {
+    res.send(data);
 });
 
 app.listen(3000, function () {
