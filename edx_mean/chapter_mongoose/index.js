c0c5641260de8c5298caf5d4b06b6e1f21bd70d9
Manash Kumar Mandal <manash@protonmail.com>
2016-12-28 02:09:11 +0600

    Add files via upload

--- edx_mean/chapter_mongoose/index.js
+++ edx_mean/chapter_mongoose/index.js
@@ -10,19 +10,32 @@ var user = new User({
     email: 'manashmndl@gmail.com'
 });
 
+var user2 = new User({
+    name: 'Mandal Manash',
+    email: 'mandalmanash@gmail.com'
+});
+
 user.save(function(error) {
     if (error) {
         console.log(error);
         process.exit(1);
     }
-    User.find({
-        email: 'manashmndl@gmail.com'
-    }, function(error, docs) {
-        if (error) {
-            console.log(error);
-            process.exit(1);
-        }
-        console.log(require('util').inspect(docs));
-        process.exit(0);
-    });
+});
+
+user2.save(function(error) {
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+});
+
+User.find({
+    email: 'mandalmanash@gmail.com'
+}, function(error, docs) {
+    if (error) {
+        console.log(error);
+        process.exit(1);
+    }
+    console.log(require('util').inspect(docs));
+    process.exit(0);
 });
